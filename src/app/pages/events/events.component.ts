54d66ad55c7adf137e02a0c351d8bd6ce3b77ef5
Luca-Favaretto <145663602+Luca-Favaretto@users.noreply.github.com>
2024-01-03 18:03:17 +0100

    fix card

--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss']
+  styleUrls: ['./events.component.scss'],
 })
 export class EventsComponent implements OnInit {
+  events: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
